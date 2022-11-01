8b9caa1525adb2cf5ccc8afd8cea00dc83c14c6c
Bart Feenstra <bart@mynameisbart.com>
2022-11-01 01:04:06 +0000

    meh

--- betty_site/release.py
+++ betty_site/release.py
@@ -1,11 +1,11 @@
-import json
+import re
 from contextlib import suppress
+from datetime import datetime
 from enum import Enum, unique
 from typing import List, Optional, Dict, Iterator, Iterable
 
+import requests
 import semver
-
-from betty_site import ASSETS_OUTPUT_WWW_DIRECTORY_PATH
 
 
 @unique
@@ -16,6 +16,12 @@ class DownloadType(Enum):
     EXECUTABLE_WINDOWS_ZIP = 'executable_windows_zip'
 
 
+_FILE_NAME_TO_DOWNLOAD_TYPE = {
+    'betty.app.zip': DownloadType.EXECUTABLE_MAC_ZIP,
+    'betty.exe.zip': DownloadType.EXECUTABLE_WINDOWS_ZIP,
+}
+
+
 class Download:
     def __init__(self, url: str, download_type: DownloadType):
         self.url = url
@@ -23,38 +29,49 @@ class Download:
 
 
 class Release:
-    def __init__(self, version: str, downloads: List[Download], stable: bool):
+    def __init__(self, version: str, date: datetime, downloads: List[Download], stable: bool):
         self.version = version
+        self.date = date
         self.downloads = {download.type: download for download in downloads}
         self.stable = stable
 
 
-_releases_data = None
-
-
-def _get_releases_data():
-    global _releases_data
-
-    if _releases_data is None:
-        with open(ASSETS_OUTPUT_WWW_DIRECTORY_PATH / 'release' / 'index.json') as f:
-            _releases_data = json.load(f)
-    return _releases_data
+_releases = None
 
 
 def get_releases() -> Dict[str, Release]:
-    return {
-        version: Release(
-            version,
-            [
-                Download(url, DownloadType(download_type))
-                for download_type, url
-                in release_data['downloads'].items()
-            ],
-            release_data.get('stable', True)
-        )
-        for version, release_data
-        in _get_releases_data().items()
-    }
+    global _releases
+
+    if _releases is None:
+        _releases = {}
+        for release_data in requests.get('https://api.github.com/repos/bartfeenstra/betty/releases?per_page=100').json():
+            if release_data['draft']:
+                continue
+
+            is_release_tag = semver.VersionInfo.isvalid(release_data['tag_name'])
+            is_dev_release = re.match(r'^\d+\.\d+\.x-dev$', release_data['tag_name'])
+
+            if not (is_release_tag or is_dev_release):
+                continue
+
+            release = Release(
+                release_data['tag_name'],
+                release_data['created_at'],
+                [
+                    Download(
+                        assets_data['browser_download_url'],
+                        DownloadType(_FILE_NAME_TO_DOWNLOAD_TYPE[assets_data['name']]),
+                    )
+                    for assets_data
+                    in release_data['assets']
+                    if assets_data['name'] in _FILE_NAME_TO_DOWNLOAD_TYPE
+                ],
+                not (release_data.get('prerelease', True) or is_dev_release)
+            )
+
+            _releases[release.version] = release
+
+    return _releases
 
 
 def get_stable_releases() -> List[Release]:
